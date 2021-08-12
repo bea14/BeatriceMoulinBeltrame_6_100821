f0c9c714a350f8a62e329310712b9925b2bc1ec8
bea14 <58847427+bea14@users.noreply.github.com>
2021-08-12 16:13:37 +0200

    Ajout du dossier models qui contient sauce et user

---
+++ backend/models/sauce.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+//création du schéma strict Sauce
+const sauceSchema = mongoose.Schema({
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: false, default: 0 },
+  dislikes: { type: Number, required: false, default: 0 },
+  usersLiked: { type: [String], required: false, defaultValue:[] },
+  usersDisliked: { type: [String], required: false, defaultValue:[] },
+});
+
+//export du schéma en tant que modèle Mongoose, le rendant disponible pour Express
+module.exports = mongoose.model('Sauce', sauceSchema);
