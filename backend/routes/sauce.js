c7dd42b00aa27a84da17f980e716e12ca48f3722
bea14 <58847427+bea14@users.noreply.github.com>
2021-08-12 16:12:53 +0200

    Ajout du dossier routes qui contient sauce et user

---
+++ backend/routes/sauce.js
@@ -0,0 +1,21 @@
+//Création du routeur express
+const express = require('express');
+//On enregistre les routes dans le routeur puis dans l'appli
+const router = express.Router();
+
+//importation du controller sauce
+const sauceCtrl = require('../controllers/sauce');
+// importtaion authentification
+const auth = require('../middleware/auth');
+// importation multer pour le telechargement des images
+const multer = require('../middleware/multer-config');
+
+// routes
+router.get('/', auth, sauceCtrl.getAllSauces);
+router.get('/:id', auth, sauceCtrl.getOneSauce);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, multer, sauceCtrl.modifySauce);
+router.delete('/:id', auth, sauceCtrl.deleteSauce);
+router.post('/:id/like', auth, sauceCtrl.likeDislikeSauce);
+
+module.exports = router;
